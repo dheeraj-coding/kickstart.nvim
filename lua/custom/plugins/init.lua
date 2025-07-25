9566a88fc84ec1820a8f4ddfd7e4109b974d2a70
Dheeraj Mohan <modheer@amazon.com>
2025-07-25 14:28:43 -0700

    add custom plugins and keymodifiers

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,28 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function()
+      vim.g.barbar_auto_setup = true
+    end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    config = function()
+      local map = vim.api.nvim_set_keymap
+      local opts = { noremap = true, silent = true }
+      map('n', 'P', '<Cmd>BufferPrevious<CR>', opts)
+      map('n', 'N', '<Cmd>BufferNext<CR>', opts)
+      map('n', '<C-x>', '<Cmd>BufferClose<CR>', opts)
+    end,
+  },
+}
